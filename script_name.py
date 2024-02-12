d5350ca4d6f11ad3b1bff4670e8306d9f393018f
ahmaddahb36 <86228627+ahmaddahb36@users.noreply.github.com>
2024-02-13 00:59:18 +0300

    Update script_name.py

--- script_name.py
+++ script_name.py
@@ -1,50 +1,123 @@
-from flask import Flask, render_template, request
+from pywebio.platform.flask import webio_view
+from pywebio import STATIC_PATH
+from flask import Flask, send_from_directory
+from pywebio.input import *
+from pywebio.output import *
+import argparse
+from pywebio import start_server
 
+import pickle
+import numpy as np
+model = pickle.load(open('regression_rf.pkl', 'rb'))
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        mark = request.form['mark']
-        med = request.form['med']
-        jobs = request.form['jobs']
-        chek = request.form['chek']
 
-        marks = {
-            "A+": 94,
-            "A": 88,
-            "A-": 84,
-            "B+": 77,
-            "B": 76,
-            "B-": 70,
-            "C+": 68,
-            "C": 63,
-            "C-": 60,
-            "D+": 56,
-            "D": 50,
-            "F": 35,
-        }
-        marks1 = [94, 88, 84, 77, 76, 70, 68, 63, 60, 56, 50, 35]
-        f = []
-        final = marks[mark] - jobs - med
-        x = marks1.index(marks[mark])
-        c = 0
-        if chek == "مدمج":
-            for i in range(marks1[x], marks1[x - 1]):
-                if final <= 40:
-                    f.append(final)
-                    c += 1
-                    final += 1
-        else:
-            for i in range(marks1[x], marks1[x - 1]):
-                if final <= 50:
-                    f.append(final)
-                    c += 1
-                    final += 1
+def predict():
+    def chek_mark(data):
+        marks = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"]
+        for i in range(12):
+            if data["mark"] not in marks:
+                return ("mark", "يرجى ادخال الرمز بالانجليزية وكبير ")
 
-        return render_template('index.html', mark=mark, med=med, jobs=jobs, chek=chek, f=f)
+    def checkMed(data):
+        if data["med"] > 30 or data["med"] < 0:
+            return ("med", "تأكد من العلامة المدخلة")
 
-    return render_template('index.html')
+    popup(
+        "موقع لحساب علامة الامتحان النهائي",
+        put_text("اهلا بكم").onclick(lambda: toast("welcome")),
+    )
+    put_html(
+        '<center><h3>حساب علامة الامتحان النهائي</h3><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqqp6Ets9gGG59SYbQqA4lhx6QNwzERnjj7A&usqp=CAU" width=200></center>'
+    )
+    put_html("<marquee>---------  حساب علامة الامتحان النهائي   --------</marquee>")
+    data = input_group(
+        "ادخل البيانات التالية ",
+        [
+            input(
+                " العلامة بالرمــوز",
+                name="mark",
+                required=True,
+            ),
+            input(
+                "علامة الامتحان النصفي",
+                name="med",
+                type=NUMBER,
+                required=True,
+            ),
+            input(
+                "علامة اعمال الفصل",
+                name="jobs",
+                type=NUMBER,
+                required=True,
+            ),
+            radio(
+                "طبيعة المادة",
+                name="chek",
+                options=["واجهي", "مدمج"],
+                required=True,
+                inline=True,
+                help_text="اذا كانت علامة اعمال الفصل من 20 ادخل وجاهي اما اذا كانت من 30ادخل مدمج",
+            ),
+        ],
+        validate=chek_mark,
+    )
+    marks = {
+        "A+": 94,
+        "A": 88,
+        "A-": 84,
+        "B+": 77,
+        "B": 76,
+        "B-": 70,
+        "C+": 68,
+        "C": 63,
+        "C-": 60,
+        "D+": 56,
+        "D": 50,
+        "F": 35,
+    }
+    marks1 = [94, 88, 84, 77, 76, 70, 68, 63, 60, 56, 50, 35]
+    f = []
+    final = marks[data["mark"]] - data["jobs"] - data["med"]
+    x = marks1.index(marks[data["mark"]])
+    c = 0
+    if data["chek"] == "مدمج":
+        for i in range(marks1[x], marks1[x - 1]):
+            if final <= 40:
+                f.insert(c, final)
+                c += 1
+                final += 1
+    else:
+        for i in range(marks1[x], marks1[x - 1]):
+            if final <= 50:
+                f.insert(c, final)
+                c += 1
+                final += 1
+    put_text("\n")
+    put_table(
+        [
+            [data["mark"], "العلامة بالرموز"],
+            [data["med"], "علامة الميد"],
+            [data["jobs"], "علامة اعمال الفصل"],
+            [data["chek"], "طبيعة المادة"],
+            [f, "العلامات المتوقعة للامتحان النهائي"],
+        ],
+    )
+    put_button("go back", onclick=lambda: run_js("location.reload()"))
+
+app.add_url_rule('/tool', 'webio_view', webio_view(predict),
+            methods=['GET', 'POST', 'OPTIONS'])
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(predict, port=args.port)
+#if __name__ == '__main__':
+    #predict()
+
+#app.run(host='localhost', port=80)
+
+#visit http://localhost/tool to open the PyWebIO application.
